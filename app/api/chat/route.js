d7870f2b64e4fe34b246d9be137f6e39b0545566
“integrateLuis” <integrate.luis@gmail.com>
2023-11-21 21:28:19 -0600

    9:28

--- app/api/chat/route.js
+++ app/api/chat/route.js
@@ -31,7 +31,7 @@ export async function POST(request) {
 
     let runStatus = await openai.beta.threads.runs.retrieve(threadId, run.id);
     while (runStatus.status !== "completed") {
-      await new Promise((resolve) => setTimeout(resolve, 15000));
+      await new Promise((resolve) => setTimeout(resolve, 2000));
       runStatus = await openai.beta.threads.runs.retrieve(threadId, run.id);
     }
 
